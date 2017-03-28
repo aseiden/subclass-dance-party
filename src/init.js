02b1eea83c035d3e7df0b0422089162a1f84be9c
MKS-Student <mks.student@hackreactor.com>
2017-03-28 12:22:12 -0700

    create nervous dancers that avoid mouse and other dancers

--- src/init.js
+++ src/init.js
@@ -37,5 +37,42 @@ $(document).ready(function() {
     }
   });
 
+  $('body').on('mouseover', '.isCalm', function() {
+    $(this).removeClass('isCalm');
+    $(this).addClass('isNervous');
+  })
+
+  setInterval(function() {
+    for (var i = 0; i < window.dancers.length; i++) {
+      if (window.dancers[i].$node.hasClass('isCalm')) {
+        for (var k = 0; k < window.dancers.length; k++) {
+          if (i !== k) {
+            var iTop = Number(window.dancers[i].top) + Number(window.dancers[i].$node.css('marginTop').slice(0, -2));
+            var iLeft = Number(window.dancers[i].left) + Number(window.dancers[i].$node.css('marginLeft').slice(0, -2));
+            var kTop = Number(window.dancers[k].top) + Number(window.dancers[k].$node.css('marginTop').slice(0, -2));
+            var kLeft = Number(window.dancers[k].left) + Number(window.dancers[k].$node.css('marginLeft').slice(0, -2));
+
+            var dv = iTop - kTop;
+            var dh = iLeft - kLeft;
+
+            var distanceSquared = (dv * dv) + (dh * dh);
+            var distance = Math.sqrt(distanceSquared);
+
+            if (distance < 75) {
+              // window.dancers[i].isNervous = true;
+              window.dancers[i].$node.removeClass('isCalm');
+              window.dancers[i].$node.addClass('isNervous');
+            }
+          }
+        }
+      }
+    }
+  }, 250);
+    //iterate over dancers
+      //if dancers[i] is nervousDancer class
+        //initiate subloop comparing nervous location to all other dancer locations (beside self)
+          //if distance is less than x
+            //set isNervous boolean to true, add class of isNervous
+
 });
 
---
+++ src/nervousDancer.js
@@ -0,0 +1,47 @@
+var NervousDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass('isCalm');
+  // this.isNervous = false;
+}
+
+NervousDancer.prototype = Object.create(Dancer.prototype);
+NervousDancer.prototype.constructor = NervousDancer;
+
+NervousDancer.prototype.step = function() {
+  Dancer.prototype.step.call(this);
+  
+  var thisDancer = this;
+
+  if (this.$node.hasClass('isNervous')) {
+    this.$node.animate({
+      marginLeft: '+=' + this.shift(),
+      marginTop: '+=' + this.shift()
+    }, this.timeBetweenSteps);
+
+    setTimeout(function(){
+      // thisDancer.isNervous = false;
+      thisDancer.$node.removeClass('isNervous');
+      thisDancer.$node.addClass('isCalm');
+    }, thisDancer.timeBetweenSteps)
+
+  } else {
+    this.$node.animate({
+      marginTop: -15,
+    }, (this.timeBetweenSteps / 2));
+
+    setTimeout(function() {
+      thisDancer.$node.animate(
+      {
+        marginTop: +15,
+      }, (thisDancer.timeBetweenSteps / 2));
+    }, thisDancer.timeBetweenSteps / 2);
+  }
+}
+
+var makeNervousDancer = function(top, left, timeBetweenSteps) {
+  return new NervousDancer(top, left, timeBetweenSteps);
+}
+
+NervousDancer.prototype.shift = function() {
+  return Math.floor((Math.random() * 200) - (Math.random() * 200));
+};
